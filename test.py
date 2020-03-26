5f810a848b84b43351ade6d740cbc941fa8c46de
luizbotelho90 <luizbotelho90@gmail.com>
2020-03-26 10:08:28 -0400

    update readme labs 2

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Essentials curso 4523")
+        self.assertRegex(result.data.decode(), "DevOps Essentials Lab 2 - 4 Linux")
 
 
 if __name__ == "__main__":
